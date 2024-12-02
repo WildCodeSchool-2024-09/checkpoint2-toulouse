6b53f7ede9e2527e4dc5d94dac0526584da8ecc5
Florentin Monteil <flo.monteil@gmail.com>
2024-12-02 09:56:29 +0100

    Algo N°1 done

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,13 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Your code here !
-  return 0;
+  let result = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      result++;
+    }
+  }
+  return result;
 }
 
 export default countLetters;
