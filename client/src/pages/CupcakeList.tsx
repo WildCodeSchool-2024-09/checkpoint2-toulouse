f05d5a71f5b99fb1e0546c4fc0ad02f0483f9716
Florentin Monteil <flo.monteil@gmail.com>
2024-12-02 11:52:43 +0100

    Cupcakes Step 2 Done

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -61,10 +61,12 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {(useLoaderData() as CupcakeArray).map((cupcake) => (
+          <li key={cupcake.id} className="cupcake-item">
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
         {/* end of block */}
       </ul>
     </>
