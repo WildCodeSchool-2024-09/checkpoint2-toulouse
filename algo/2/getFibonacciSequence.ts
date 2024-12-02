e1ec6294bc2d5c7a1331eccbe308b7f25242c050
Florentin Monteil <flo.monteil@gmail.com>
2024-12-02 10:17:58 +0100

    Algo N°2 done

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,17 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Your code here !
-  return [0, 1];
+  if (size <= 0) {
+    return [];
+  }
+  if (size === 1) {
+    return [0];
+  }
+  const fibonacciSequence: number[] = [0, 1];
+  for (let i = 2; i < size; i++) {
+    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2]);
+  }
+  return fibonacciSequence;
 }
 
 export default getFibonacciSequence;
